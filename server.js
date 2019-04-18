85fd229beb220439c78738716ccbf86540181d44
Igor <igorrebeche@gmail.com>
2019-04-18 15:57:09 -0300

    instalando modulos do firebase

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var cors = require('cors');
+var admin = require("firebase-admin");
 var app = express();
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://stonedesafio.firebaseio.com"
+});
+
+
+var db = admin.database();
+var ref = db.ref("/users");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
 
 app.get('/', cors(), function (req, res) {
   res.json('{Hello World: "Teste"}');
