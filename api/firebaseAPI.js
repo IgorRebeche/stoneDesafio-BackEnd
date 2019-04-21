fa2a57291cab2db5753ea39c7e6bcb6c30642a29
Igor Rebeche Rosa <igorrebeche@gmail.com>
2019-04-21 17:48:48 -0300

    corrigido integracoes com bd

--- api/firebaseAPI.js
+++ api/firebaseAPI.js
@@ -2,7 +2,18 @@ var app = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var router = app.Router();
-router.use(cors());
+
+
+var corsOptions = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,OPTION,POST,DELETE",
+  "allowedHeaders": ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
+  "preflightContinue": true,
+  "optionsSuccessStatus": 204
+}
+router.options('*', cors());
+
+router.use(cors(corsOptions));
 router.use(bodyParser.urlencoded({
   extended: false
 }));
@@ -41,8 +52,26 @@ router.get('/users', (req, res) => {
 //Verificar se ja existe o usuario (pela matricula)
 router.post('/users/add', (req, res) => {
   var userResponse = req.body;
-  console.log(userResponse)
-  userRef.child(userResponse.matricula).set(userResponse);
+
+  userRef.child(userResponse.matricula).on("value", function(snapshot) {
+    if(snapshot.exists){
+      
+      userRef.child(userResponse.matricula).set(userResponse, (error) => {
+        if(error){
+          status = {status: "exist"};
+          res.json(status);
+        }else{
+          status = {status: "sent"};
+          res.json(status);
+        }
+      });
+
+    }
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
+  
 
 });
 //Rota para Deletar usuarios
@@ -51,15 +80,27 @@ router.post('/users/remove', (req, res) => {
   var userResponse = req.body;
   console.log('Removendo: ' + userResponse.nome);
   userRef.child(userResponse.matricula).remove();
+  res.json(userResponse);
 
 });
 
 //Rota para dar update nos usuarios
 //Atualizar informações do usuario
+//router.options('/users/update', cors());
 router.post('/users/update', (req, res) => {
   var userResponse = req.body;
   console.log(userResponse)
-  userRef.child(userResponse.matricula).update(userResponse);
+  userRef.child(userResponse.matricula).update(userResponse, (error) => {
+    if(error){
+      status = {status: "error"};
+      res.json(status);
+      console.log('{"status":"error"}');
+    }else{
+      status = {status: "sent"};
+      res.json(status);
+    }
+  });
+  
 
 });
 
